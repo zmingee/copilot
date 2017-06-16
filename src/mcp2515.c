7304559202dbeff00ba6f7af45827d515d3a8cf6
Zane Mingee <zmingee@outlook.com>
2017-06-16 01:41:34 -0400

    [DOC] Added some documentation with Doxygen

--- src/mcp2515.c
+++ src/mcp2515.c
@@ -1,3 +1,19 @@
+/**
+ * @file mcp2515.c
+ * @brief Source file for interfacing with the MCP2515 module
+ *
+ * This is the implementation for the MCP2515 module. Includes all required
+ * logic for utilizing the module.
+ *
+ * @author Zane Mingee (breakingmatter)
+ * @bug UNDER ACTIVE DEVELOPMENT
+ */
+
+/** @addtogroup mcp2515
+ * @{
+ */
+
+
 #include <avr/io.h>
 
 #include "spi.h"
@@ -5,13 +21,27 @@
 
 static int SPI_CS;
 
-void mcp2515_reset(void) {
+/**
+ * @brief Reset the MCP2515.
+ *        This just writes the MCP_RESET macro define to the SPI port.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ */
+static void mcp2515_reset(void) {
     PORTB &= ~(1 << SPI_CS);
     spi_transmit(MCP_RESET);
     PORTB |= (1 << SPI_CS);
 }
 
-void mcp2515_set_register(const unsigned char address,
+/**
+ * @brief Set a given register to a given value.
+ *        Writes MCP_WRITE to SPI port, then writes the address and value.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param address   Address to set
+ * @param value     Value to set the register to
+ */
+static void mcp2515_set_register(const unsigned char address,
         const unsigned char value) {
     PORTB &= ~(1 << SPI_CS);
     spi_transmit(MCP_WRITE);
@@ -20,7 +50,16 @@ void mcp2515_set_register(const unsigned char address,
     PORTB |= (1 << SPI_CS);
 }
 
-unsigned char mcp2515_read_register(unsigned char address) {
+/**
+ * @brief Read the value of a register.
+ *        Writes MCP_READ to the SPI port, writes the address, then writes 0x00
+ *        and returns the response.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param address   Address to read
+ * @return SPI response from MCP2515
+ */
+static unsigned char mcp2515_read_register(unsigned char address) {
     unsigned char ret;
 
     PORTB &= ~(1 << SPI_CS);
@@ -32,7 +71,17 @@ unsigned char mcp2515_read_register(unsigned char address) {
     return ret;
 }
 
-void mcp2515_modify_register(unsigned char address, unsigned char mask,
+/**
+ * @brief Modify a register using the mask specified.
+ *        Writes MCP_BITMOD to the SPI port, writes the address, the mask,
+ *        and the data to submit.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param address   Register address to modify
+ * @param mask      Mask to use when manipulating the register value
+ * @param data      Data to assign to the register
+ */
+static void mcp2515_modify_register(unsigned char address, unsigned char mask,
         unsigned char data) {
     PORTB &= ~(1 << SPI_CS);
     spi_transmit(MCP_BITMOD);
@@ -42,7 +91,16 @@ void mcp2515_modify_register(unsigned char address, unsigned char mask,
     PORTB |= (1 << SPI_CS);
 }
 
-unsigned char mcp2515_set_CANCTRL_Mode(unsigned char newmode) {
+/**
+ * @brief This function will set the MCP2515 mode.
+ *        Modifies the MCP_CANCTRL register, then reads the register and
+ *        verifies it was successful.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param newmode   Mode to set the MCP2515 to
+ * @return Message indicating success or failure
+ */
+static unsigned char mcp2515_set_CANCTRL_Mode(unsigned char newmode) {
     unsigned char i;
 
     mcp2515_modify_register(MCP_CANCTRL, MODE_MASK, newmode);
@@ -57,7 +115,17 @@ unsigned char mcp2515_set_CANCTRL_Mode(unsigned char newmode) {
     return MCP2515_FAIL;
 }
 
-unsigned char mcp2515_config_rate(const unsigned char can_speed) {
+/**
+ * @brief Set the MCP2515 CAN speed.
+ *        Modifies the MCP_CNF1, MCP_CNF2, and MCP_CNF3 registers to the
+ *        corresponding values for the specified speeds, then returns a success
+ *        or failure message.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param can_speed     Speed to set the CAN bus to
+ * @return Message indicating success or failure
+ */
+static unsigned char mcp2515_config_rate(const unsigned char can_speed) {
     unsigned char set, cfg1, cfg2, cfg3;
     set = 1;
 
@@ -185,7 +253,15 @@ unsigned char mcp2515_config_rate(const unsigned char can_speed) {
     }
 }
 
-void mcp2515_init_CAN_buffers(void) {
+/**
+ * @brief Initialize the MCP2515 CAN buffers.
+ *        Walks through 0-13 and sets the MCP_TXB0CTRL, MCP_TXB1CTRL, and
+ *        MCP_TXB2CTRL registers to the corresponding integers. Then, sets the
+ *        MCP_RXB0CTRL and MCP_RXB1CTRL registers to 0.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ */
+static void mcp2515_init_CAN_buffers(void) {
     unsigned char i;
     unsigned char a1 = MCP_TXB0CTRL;
     unsigned char a2 = MCP_TXB1CTRL;
@@ -203,6 +279,17 @@ void mcp2515_init_CAN_buffers(void) {
     mcp2515_set_register(MCP_RXB1CTRL, 0);
 }
 
+/**
+ * @brief Initialize the MCP2515
+ *        This will set put the MCP2515 into config mode, set the CAN speed,
+ *        initialize the CAN buffers, setup the interrupt mode, and then put
+ *        the MCP2515 back into normal operating mode.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param port          AVR port to use for SPI slave/chip select
+ * @param can_speed     Speed to set the CAN bus to
+ * @return Message indicating success or failure
+ */
 unsigned char mcp2515_init(unsigned int port, unsigned char can_speed) {
     unsigned char res;
 
@@ -248,6 +335,13 @@ unsigned char mcp2515_init(unsigned int port, unsigned char can_speed) {
     return CAN_OK;
 }
 
+/**
+ * @brief Read the current status of the MCP2515.
+ *        Writes MCP_READ_STATUS to SPI and returns the response.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @return SPI response from MCP2515
+ */
 unsigned char mcp2515_read_status(void) {
     unsigned char i;
 
@@ -259,6 +353,14 @@ unsigned char mcp2515_read_status(void) {
     return i;
 }
 
+/**
+ * @brief Check if there are any CAN messages to receive.
+ *        Reads the status of the MCP2515 module and interprets if a CAN
+ *        message is available.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @return Message indicating if a message is available
+ */
 unsigned char mcp2515_check_receive(void) {
     unsigned char res;
 
@@ -269,3 +371,5 @@ unsigned char mcp2515_check_receive(void) {
         return CAN_NOMSG;
     }
 }
+
+/** @}*/
--- src/mcp2515.h
+++ src/mcp2515.h
@@ -1,17 +1,50 @@
-#ifndef MCP2515_H
-#define MCP2515_H
+/**
+ * @file mcp2515.h
+ * @brief Function prototypes for interfacing with the MCP2515 module
+ *
+ * This contains the prototypes and API for interfacing with the MCP2515
+ * module, as well as any macros, constants, and global variables you may
+ * require.
+ *
+ * @author Zane Mingee (breakingmatter)
+ * @bug UNDER ACTIVE DEVELOPMENT
+ */
 
-void mcp2515_reset(void);
+/** @addtogroup mcp2515
+ * @{
+ */
 
-unsigned char mcp2515_read_register(unsigned char address);
+#ifndef _MCP2515_H
+#define _MCP2515_H
 
-void mcp2515_modify_register(unsigned char address, unsigned char mask,
-        unsigned char data);
-
-unsigned char mcp2515_set_CANCTRL_Mode(unsigned char newmode);
-
+/**
+ * @brief Initialize the MCP2515
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @param port          AVR port to use for SPI slave/chip select
+ * @param can_speed     Speed to set the CAN bus to
+ * @return Message indicating success or failure
+ */
 unsigned char mcp2515_init(unsigned int port, unsigned char can_speed);
 
+/**
+ * @brief Read the current status of the MCP2515
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @return SPI response from MCP2515
+ */
+unsigned char mcp2515_read_status(void);
+
+/**
+ * @brief Check if there are any CAN messages to receive.
+ *        Reads the status of the MCP2515 module and interprets if a CAN
+ *        message is available.
+ * @author Zane Mingee
+ * @date 2017-06-15
+ * @return Message indicating if a message is available
+ */
 unsigned char mcp2515_check_receive(void);
 
-#endif
+#endif /* _MCP2515_H */
+
+/** @}*/
