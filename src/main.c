df733e34c9a1dba12c4bf8ef2555ebc70e320631
Zane Mingee <zmingee@outlook.com>
2017-09-24 00:00:23 -0400

    [REF] Refactored main file to have more structure

--- src/main.c
+++ src/main.c
@@ -1,34 +1,109 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <util/delay.h>
+#include <stdio.h>
 
+#include "log.h"
 #include "uart.h"
 #include "spi.h"
 #include "mcp2515.h"
 #include "mcp_can_dfs.h"
 
+FILE uart_str = FDEV_SETUP_STREAM(uart_write_c, NULL, _FDEV_SETUP_RW);
+volatile unsigned long timer1_millis;
+#define CTC_MATCH_OVERFLOW ((F_CPU / 1000) / 8)
 
-int main(void) {
-    unsigned char res;
+ISR(TIMER1_COMPA_vect) {
+    timer1_millis++;
+}
 
-    /* Set up the serial port */
+unsigned long millis() {
+    unsigned long millis_return;
+
+    ATOMIC_BLOCK(ATOMIC_FORCEON) {
+        millis_return = timer1_millis;
+    }
+
+    return millis_return;
+}
+
+void setup_timer() {
+    /* Setup the timer for logging */
+    TCCR1B |= (1 << WGM12) | (1 << CS11);
+    OCR1AH = (CTC_MATCH_OVERFLOW >> 8);
+    OCR1AL = CTC_MATCH_OVERFLOW;
+    TIMSK1 |= (1 << OCIE1A);
+    sei();
+}
+
+bool boot(){
+    /* Setup the serial port */
     uart_init();
+    uart_write("Booting copilot...\r\n");
+    uart_write("UART configured\r\n");
+    stdout = &uart_str;
+    printf("Setup UART as STDOUT\r\n");
 
-    /* The Arduino is now booted up and ready. */
-    uart_write("Boot OK\r\n");
+    setup_timer();
+    printf("Setup global interupts and timer on TIMER1\r\n");
+
+    LOG_PRINT(DEBUG, "Setup system ready");
 
     /* Set up SPI */
     spi_init();
-    uart_write("SPI initialized\r\n");
+    LOG_PRINT(DEBUG, "SPI initialized");
+
+    LOG_PRINT(DEBUG, "Boot successful");
+
+    return true;
+}
+
+int main(void) {
+    stdout = &uart_str;
+    unsigned char res;
+    bool boot_status;
+
+    boot_status = boot();
+    if (!boot_status) {
+        LOG_PRINT(ERROR, "Boot initialization failed!");
+        return 0;
+    }
+
 
     /* Set up mcp2515 */
     while (1) {
         res = mcp2515_init(PB1, CAN_500KBPS);
         if (res == CAN_OK) {
-            uart_write("MCP initialized\r\n");
+            LOG_PRINT(INFO, "MCP initialized");
             break;
         } else {
-            uart_write("Failed to initialize MCP\r\n");
-            uart_write("Resetting\r\n");
+            LOG_PRINT(INFO, "Failed to initialize MCP");
+            LOG_PRINT(INFO, "Resetting");
+            _delay_ms(1000);
+        }
+    }
+
+    struct CanMessage *msg = NULL;
+    while (1) {
+        if (mcp2515_check_receive() == CAN_MSGAVAIL) {
+            LOG_PRINT(INFO, "CAN message receieved");
+            mcp2515_read_msg(msg);
+            if (msg->data == NULL) {
+            } else {
+                printf("---\r\n");
+            }
+            for (unsigned int i = 0; i < msg->data_len; i++) {
+                printf("0x%02X", msg->data[i]);
+            }
+            free(msg);
+            printf("\r\n");
+            _delay_ms(100);
+        } else {
+            LOG_PRINT(INFO, "No message available");
+            _delay_ms(1000);
         }
     }
 
